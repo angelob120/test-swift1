adbd58c8806c679e7f190a743caf174fe43358c1
Angelo Brown <130711987+angelob120@users.noreply.github.com>
2023-10-18 15:45:10 -0400

    Initial Commit

---
+++ k/kApp.swift
@@ -0,0 +1,17 @@
+//
+//  kApp.swift
+//  k
+//
+//  Created by Angelo Brown on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct kApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
